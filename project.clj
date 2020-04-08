69c5af1cf8336521dc01614003e0e7494c6895e9
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-04-08 15:27:44 -0300

    chore: Add project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject lffg "0.0.0"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns infosys.core}
+  :plugins [[cider/cider-nrepl "0.22.1"] [lein-cljfmt "0.6.7"]])
