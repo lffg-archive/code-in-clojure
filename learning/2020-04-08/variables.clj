f6d4d461248b49265e3f9784d894b0f0ffc64d19
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-04-08 15:27:50 -0300

    feat: Add variables example

---
+++ learning/2020-04-08/variables.clj
@@ -0,0 +1,9 @@
+; `def` is used to declare "variables".
+; In line 4, for example, the value `1` is associated to the `x` symbol.
+
+(def x 1)
+(def y 2)
+(def x+y (+ x y))
+(def out (<< "The sum of " x " and " y " is " x+y "."))
+
+(println out)
